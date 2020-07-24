277b4e65b7e90c74746c00f47e83b29bd64c4b4c
superbkibbles <mend_mzury@hotmail.com>
2020-07-24 15:48:13 +0300

    header

--- src/components/HeaderFooter/Header/Head.js
+++ src/components/HeaderFooter/Header/Head.js
@@ -11,25 +11,31 @@ export default () => {
 
     return (
         <Container fluid className='header'>
-            <Row>
-                <Col>
-                    <div className='header__logo'>
-                        <Logo width={'23.5rem'} height={'10rem'} />
-                    </div>
-                </Col>
-                <Col>
-                    <SearchBox inputChange={inputChange} />
-                </Col>
-                <Col>
-                    <ul className='header__contact text-right'>
-                        <li className='header__contact-li'>
-                            <a href="tel:+964-750-397-8938" className='btn-text'>+9647503978938</a>
-                        </li>
-                        <li className='header__contact-li'>time of work</li>
-                        <li className='header__contact-li'>8:30 am - 7:30pm</li>
-                    </ul>
-                </Col>
-            </Row>
+            <div className='header__desktop'>
+                <Row>
+                    <Col>
+                        <div className='header__logo'>
+                            <Logo width={'23.5rem'} height={'10rem'} />
+                        </div>
+                    </Col>
+                    <Col>
+                        <SearchBox inputChange={inputChange} />
+                    </Col>
+                    <Col>
+                        <ul className='header__contact text-right'>
+                            <li className='header__contact-li'>
+                                <a href="tel:+964-750-397-8938" className='btn-text'>+9647503978938</a>
+                            </li>
+                            <li className='header__contact-li'>time of work</li>
+                            <li className='header__contact-li'>8:30 am - 7:30pm</li>
+                        </ul>
+                    </Col>
+                </Row>
+            </div>
+
+            <div className='header__mobile'>
+                mobile version
+            </div>
         </Container>
     )
 }
