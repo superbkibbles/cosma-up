9a36a2f93a93024f659180da8162ea6bd9fced8c
superbkibbles <mend_mzury@hotmail.com>
2020-07-24 13:48:11 +0300

    Updates

---
+++ precache-manifest.a38615e83a7584a727fa5c2f628fbf40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed871ab1886e9d1149114f5175c2c48",
+    "url": "/cosma-up/index.html"
+  },
+  {
+    "revision": "1c24dcbdfd49598c3e48",
+    "url": "/cosma-up/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dc5d6238715cecaae387",
+    "url": "/cosma-up/static/css/main.8087241b.chunk.css"
+  },
+  {
+    "revision": "1c24dcbdfd49598c3e48",
+    "url": "/cosma-up/static/js/2.1799d38d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cosma-up/static/js/2.1799d38d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc5d6238715cecaae387",
+    "url": "/cosma-up/static/js/main.ec440fb3.chunk.js"
+  },
+  {
+    "revision": "48ec4b1402778dcd46f7",
+    "url": "/cosma-up/static/js/runtime-main.9f9083ab.js"
+  }
+]);
